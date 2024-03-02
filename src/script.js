35fed27f80abdd57de0e4e638330efa203fdb6e2
tronza31 <tronza31@mail.ru>
2024-03-02 12:18:05 +0300

    Fixed branch protection rule. Closed #13

--- src/script.js
+++ src/script.js
@@ -1,50 +1,91 @@
 class MessageBox
 {
-    constructor(divId, message)
+  constructor(divId, message)
+  {
+    this.divId = divId;
+    this.message = message;
+  }
+
+  PrintMessage()
+  {
+    let div = document.getElementsByClassName(this.divId)[0];
+
+    if (div !== null && div !== undefined)
     {
-        this.divId = divId;
-        this.message = message;
-    }
-
-    PrintMessage()
-    {
-        let div = document.getElementsByClassName(this.divId)[0];
-
-        if (div !== null && div !== undefined)
-        {
-            let message = document.createElement("p");
-            message.innerText = this.message;
-            div.appendChild(message); 
-        }
-        else
-        {
-            console.log("Container not founded");
-        }
-    }
-}
-
-function AddTextContainer()
-{
-    if (navigator.geolocation)
-    {
-        navigator.geolocation.watchPosition(
-            function (position)
-            {
-                console.log(position);
-
-                const CONTAINER_CLASS = "container";
-                const MESSAGE = `${position.coords.latitude}, ${position.coords.longitude}`;
-
-                let messageBox = new MessageBox(CONTAINER_CLASS, MESSAGE);
-                messageBox.PrintMessage();
-            },
-            function (err)
-            {
-                alert('Вы запретили местоположение');
-            });
+      let message = document.createElement("p");
+      message.innerText = this.message;
+      div.appendChild(message);
     }
     else
     {
-        alert("У Вас отстутсвует местоположение");
+      console.log("Container not founded");
     }
+  }
 }
+
+var watchId = null;
+
+function AddTextContainer()
+{
+  if (navigator.geolocation)
+  {
+    watchId = navigator.geolocation.watchPosition(
+      function (position)
+      {
+        const CONTAINER_CLASS = "container";
+        const MESSAGE = `${position.coords.latitude}, ${position.coords.longitude}`;
+
+        console.log(position);
+
+        let messageBox = new MessageBox(CONTAINER_CLASS, MESSAGE);
+        messageBox.PrintMessage();
+      },
+      function (error)
+      {
+        alert("Вы запретили местоположение");
+      },
+    );
+  }
+  else
+  {
+    alert("У Вас отстутсвует местоположение");
+  }
+}
+
+var buttonHtml = document.getElementById("action-button");
+
+function ChangeButtonColor()
+{
+  if (buttonHtml.classList.contains("btn-primary"))
+  {
+    buttonHtml.classList.remove("btn-primary");
+    buttonHtml.classList.add("btn-danger");
+    buttonHtml.innerText = "Stop";
+  }
+  else
+  {
+    buttonHtml.classList.remove("btn-danger");
+    buttonHtml.classList.add("btn-primary");
+    buttonHtml.innerText = "Start";
+  }
+}
+
+function StartGeoCoordinateWatching()
+{
+  buttonHtml.removeEventListener("click", StartGeoCoordinateWatching);
+  buttonHtml.addEventListener("click", StopGeoCoordinateWatching);
+  ChangeButtonColor();
+  AddTextContainer();
+  alert("Watching was run");
+}
+
+function StopGeoCoordinateWatching()
+{
+  buttonHtml.removeEventListener("click", StopGeoCoordinateWatching);
+  buttonHtml.addEventListener("click", StartGeoCoordinateWatching);
+  ChangeButtonColor();
+  navigator.geolocation.clearWatch(watchId);
+  alert("Watching was stop");
+}
+
+buttonHtml.addEventListener("click", StartGeoCoordinateWatching);
