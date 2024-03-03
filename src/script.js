231740b6b9bb67dea4533093cd0fa04d1184e862
Roman-Popov-2021 <roman.popov121212@gmail.com>
2024-03-03 13:25:30 +0300

    Added map display. Closed #15

--- src/script.js
+++ src/script.js
@@ -1,93 +1,122 @@
 class MessageBox
 {
-  constructor(divId, message)
-  {
-    this.divId = divId;
-    this.message = message;
-  }
+	constructor(divId, message)
+	{
+		this.divId = divId;
+		this.message = message;
+	}
 
-  PrintMessage()
-  {
-    let div = document.getElementsByClassName(this.divId)[0];
+	PrintMessage()
+	{
+		let div = document.getElementsByClassName(this.divId)[0];
 
-    if (div !== null && div !== undefined)
-    {
-      let message = document.createElement("p");
-      message.innerText = this.message;
-      div.appendChild(message);
-    }
-    else
-    {
-      console.log("Container not founded");
-    }
-  }
+		if (div !== null && div !== undefined)
+		{
+			let message = document.createElement("p");
+			message.innerText = this.message;
+			div.appendChild(message);
+		}
+		else
+		{
+			console.log("Container not founded");
+		}
+	}
+}
+
+class Map
+{
+	constructor(divId)
+	{
+		this.divId = divId;
+	}
+
+	InitMap()
+	{
+		navigator.geolocation.getCurrentPosition(
+			function (currentPosition)
+			{
+				var yandexMap = new ymaps.Map("map-container",
+					{
+						center: [currentPosition.coords.latitude, currentPosition.coords.longitude],
+						zoom: 15
+					}
+				);
+
+				yandexMap.setType('yandex#map');
+			}
+		);
+	}
 }
 
 var watchId = null;
 
 function AddTextContainer()
 {
-  if (navigator.geolocation)
-  {
-    watchId = navigator.geolocation.watchPosition(
-      function (position)
-      {
-        const CONTAINER_CLASS = "container";
-        const MESSAGE = `${position.coords.latitude}, ${position.coords.longitude}`;
+	if (navigator.geolocation)
+	{
+		watchId = navigator.geolocation.watchPosition(
+			function (position)
+			{
+				const CONTAINER_CLASS = "container";
+				const MESSAGE = `${position.coords.latitude}, ${position.coords.longitude}`;
 
-        let messageBox = new MessageBox(CONTAINER_CLASS, MESSAGE);
-        messageBox.PrintMessage();
-      },
-      function (error)
-      {
-        console.log(error);
-        alert("Вы запретили местоположение");
-      },
-    );
-  }
-  else
-  {
-    alert("У Вас отстутсвует местоположение");
-  }
+				let messageBox = new MessageBox(CONTAINER_CLASS, MESSAGE);
+				messageBox.PrintMessage();
+			},
+			function (error)
+			{
+				console.log(error);
+				alert("Вы запретили местоположение");
+			},
+		);
+	}
+	else
+	{
+		alert("У Вас отстутсвует местоположение");
+	}
 }
 
 var buttonHtml = document.getElementById("action-button");
 
 function ChangeButtonColor()
 {
-  if (buttonHtml.classList.contains("btn-primary"))
-  {
-    buttonHtml.classList.remove("btn-primary");
-    buttonHtml.classList.add("btn-danger");
-    buttonHtml.innerText = "Stop";
-  }
-  else
-  {
-    buttonHtml.classList.remove("btn-danger");
-    buttonHtml.classList.add("btn-primary");
-    buttonHtml.innerText = "Start";
-  }
+	if (buttonHtml.classList.contains("btn-primary"))
+	{
+		buttonHtml.classList.remove("btn-primary");
+		buttonHtml.classList.add("btn-danger");
+		buttonHtml.innerText = "Stop";
+	}
+	else
+	{
+		buttonHtml.classList.remove("btn-danger");
+		buttonHtml.classList.add("btn-primary");
+		buttonHtml.innerText = "Start";
+	}
 }
 
 function StartGeoCoordinateWatching()
 {
-  buttonHtml.removeEventListener("click", StartGeoCoordinateWatching);
-  buttonHtml.addEventListener("click", StopGeoCoordinateWatching);
-  ChangeButtonColor();
-  AddTextContainer();
-  alert("Watching was run");
+	buttonHtml.removeEventListener("click", StartGeoCoordinateWatching);
+	buttonHtml.addEventListener("click", StopGeoCoordinateWatching);
+	ChangeButtonColor();
+	AddTextContainer();
+	alert("Watching was run");
 }
 
 function StopGeoCoordinateWatching()
 {
-  buttonHtml.removeEventListener("click", StopGeoCoordinateWatching);
-  buttonHtml.addEventListener("click", StartGeoCoordinateWatching);
-  ChangeButtonColor();
-  if (watchId !== null)
-  {
-    navigator.geolocation.clearWatch(watchId);
-  }
-  alert("Watching was stop");
+	buttonHtml.removeEventListener("click", StopGeoCoordinateWatching);
+	buttonHtml.addEventListener("click", StartGeoCoordinateWatching);
+	ChangeButtonColor();
+	if (watchId !== null)
+	{
+		navigator.geolocation.clearWatch(watchId);
+	}
+	alert("Watching was stop");
 }
 
 buttonHtml.addEventListener("click", StartGeoCoordinateWatching);
+
+const MAP_DIV_ID = "map-container";
+let myMap = new Map(MAP_DIV_ID);
+ymaps.ready(myMap.InitMap);
