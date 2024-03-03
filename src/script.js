af1876d151b5be68fa70a9ddb2098b5a979ed378
tronza31 <tronza31@mail.ru>
2024-03-03 13:45:22 +0300

    Br br br

--- src/script.js
+++ src/script.js
@@ -1,93 +1,115 @@
 class MessageBox
 {
-  constructor(divId, message)
-  {
-    this.divId = divId;
-    this.message = message;
-  }
+	constructor(divId, message)
+	{
+		this.divId = divId;
+		this.message = message;
+	}
 
-  PrintMessage()
-  {
-    let div = document.getElementsByClassName(this.divId)[0];
+	PrintMessage()
+	{
+		let div = document.getElementsByClassName(this.divId)[0];
 
-    if (div !== null && div !== undefined)
-    {
-      let message = document.createElement("p");
-      message.innerText = this.message;
-      div.appendChild(message);
-    }
-    else
-    {
-      console.log("Container not founded");
-    }
-  }
+		if (div !== null && div !== undefined)
+		{
+
+			let message = document.createElement("p");
+			message.innerText = this.message;
+			div.appendChild(message);
+		}
+		else
+		{
+			console.log("Container not founded");
+		}
+	}
 }
 
-var watchId = null;
+class CoordsContainer
+{
+	constructor()
+	{
+		this.array = [];
+	}
 
+	AddCoord(coord)
+	{
+		if (this.array.lastIndexOf[0] !== coord)
+		{
+			console.log(coord);
+			this.array.push(coord);
+		}
+	}
+}
+
+var contain = null;
+var watchId = null;
 function AddTextContainer()
 {
-  if (navigator.geolocation)
-  {
-    watchId = navigator.geolocation.watchPosition(
-      function (position)
-      {
-        const CONTAINER_CLASS = "container";
-        const MESSAGE = `${position.coords.latitude}, ${position.coords.longitude}`;
+	if (navigator.geolocation)
+	{
+		watchId = navigator.geolocation.watchPosition(
+			function (position)
+			{
+				const CONTAINER_CLASS = "container";
+				const MESSAGE = `${position.coords.latitude}, ${position.coords.longitude}`;
 
-        let messageBox = new MessageBox(CONTAINER_CLASS, MESSAGE);
-        messageBox.PrintMessage();
-      },
-      function (error)
-      {
-        console.log(error);
-        alert("Вы запретили местоположение");
-      },
-    );
-  }
-  else
-  {
-    alert("У Вас отстутсвует местоположение");
-  }
+				contain = new CoordsContainer();
+				contain.AddCoord(MESSAGE);
+
+				let messageBox = new MessageBox(CONTAINER_CLASS, MESSAGE);
+				messageBox.PrintMessage();
+			},
+			function (error)
+			{
+				console.log(error);
+				alert("Вы запретили местоположение");
+			},
+		);
+	}
+	else
+	{
+		alert("У Вас отстутсвует местоположение");
+	}
 }
 
 var buttonHtml = document.getElementById("action-button");
 
 function ChangeButtonColor()
 {
-  if (buttonHtml.classList.contains("btn-primary"))
-  {
-    buttonHtml.classList.remove("btn-primary");
-    buttonHtml.classList.add("btn-danger");
-    buttonHtml.innerText = "Stop";
-  }
-  else
-  {
-    buttonHtml.classList.remove("btn-danger");
-    buttonHtml.classList.add("btn-primary");
-    buttonHtml.innerText = "Start";
-  }
+	if (buttonHtml.classList.contains("btn-primary"))
+	{
+		buttonHtml.classList.remove("btn-primary");
+		buttonHtml.classList.add("btn-danger");
+		buttonHtml.innerText = "Stop";
+	}
+	else
+	{
+		buttonHtml.classList.remove("btn-danger");
+		buttonHtml.classList.add("btn-primary");
+		buttonHtml.innerText = "Start";
+	}
 }
 
 function StartGeoCoordinateWatching()
 {
-  buttonHtml.removeEventListener("click", StartGeoCoordinateWatching);
-  buttonHtml.addEventListener("click", StopGeoCoordinateWatching);
-  ChangeButtonColor();
-  AddTextContainer();
-  alert("Watching was run");
+	buttonHtml.removeEventListener("click", StartGeoCoordinateWatching);
+	buttonHtml.addEventListener("click", StopGeoCoordinateWatching);
+	ChangeButtonColor();
+	AddTextContainer();
+	alert("Watching was run");
 }
 
 function StopGeoCoordinateWatching()
 {
-  buttonHtml.removeEventListener("click", StopGeoCoordinateWatching);
-  buttonHtml.addEventListener("click", StartGeoCoordinateWatching);
-  ChangeButtonColor();
-  if (watchId !== null)
-  {
-    navigator.geolocation.clearWatch(watchId);
-  }
-  alert("Watching was stop");
+	buttonHtml.removeEventListener("click", StopGeoCoordinateWatching);
+	buttonHtml.addEventListener("click", StartGeoCoordinateWatching);
+	ChangeButtonColor();
+	if (watchId !== null)
+	{
+		navigator.geolocation.clearWatch(watchId);
+	}
+	alert("Watching was stop");
+	console.log(contain.array);
 }
 
 buttonHtml.addEventListener("click", StartGeoCoordinateWatching);
